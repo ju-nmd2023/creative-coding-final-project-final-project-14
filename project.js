41907064f14f6e1c108d0457f30127d21826ef27
adve23wf <adve23wf@student.ju.se>
2025-10-08 20:01:34 +0200

    Connecting random notes art with perlin nose art

---
+++ project.js
@@ -0,0 +1,64 @@
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  frameRate(10);
+}
+let synth;
+
+window.addEventListener("load", () => {
+  synth = new Tone.Synth({
+    oscillator: { type: "sine" },
+    envelope: { attack: 0.1, release: 0.2 },
+  }).toDestination();
+
+  const keys = [
+    "A3",
+    "A4",
+    "C3",
+    "C4",
+    "E3",
+    "E4",
+    "G3",
+    "G4",
+    "B3",
+    "B4",
+    "D3",
+    "D4",
+    "F3",
+    "F4",
+  ];
+
+  loop = new Tone.Loop((time) => {
+    const key = random(keys);
+    synth.triggerAttackRelease(key, time);
+
+    color = random(20, 250);
+  }, "5n");
+  Tone.Transport.start();
+  loop.start(0);
+});
+
+const size = 10;
+const divider = 10;
+const numRows = 170;
+const numCols = 170;
+
+let counter = 10;
+
+function draw() {
+  background(0);
+  noiseSeed(1);
+
+  const startX = width / 2 - (numCols * size) / 2;
+  const startY = height / 2 - (numRows * size) / 2;
+
+  //The following code was made with inspiration from the noise tutorial shown during one of the lectures.
+  for (let y = 0; y < numRows; y++) {
+    for (let x = 0; x < numCols; x++) {
+      const value = noise(x / divider, y / divider, counter) * size;
+      fill(color, 0, map(value, 0, 1, 70, 270));
+      square(startX + x * size, startY + y * size, value);
+    }
+  }
+
+  counter += 0.05;
+}
