46a754fa1af4ac28746e39131d49f047acd1b2b2
adve23wf <adve23wf@student.ju.se>
2025-10-18 15:53:13 +0200

    Adding growing to the art according to clicks

--- project.js
+++ project.js
@@ -1,29 +1,28 @@
 function setup() {
   createCanvas(innerWidth, innerHeight);
   frameRate(10);
-
-  //size = 5;
-  // numCols = ceil(width / size);
-  //  numRows = ceil(height / size);
 }
 let numCols;
 let numRows;
-
+let size;
 let synth;
 let pulse = 0;
 let mic;
 let meter;
-//const size = 10;
-//const numRows = 170;
-//const numCols = 170;
-let counter = 2;
-let currentColor;
-let targetColor;
-let transitionSpeed = 0.05;
-let interA;
-let interB;
+let level;
+let counter = 0;
+let noiseScale = 0.5;
+let divider;
+let startX, startY;
+let x, y;
+let offsetX, offsetY;
+let c;
+let newArt = false;
+let nextArt = false;
+let lastArt = false;
+let shuffel = 100;
 
-const soundTypes = ["sine2", "fatsine", "fatsawtooth", "pulse", "fmsquare"];
+const soundTypes = ["sine2", "fatsawtooth", "pulse", "fmsquare"];
 let currentSound = 0;
 
 //----LOAD ------------
@@ -53,7 +52,7 @@ window.addEventListener("load", () => {
   loop = new Tone.Loop((time) => {
     const key = random(keys);
     synth.triggerAttackRelease(key, "8n", time);
-    color = random(0, 100);
+    c = random(0, 100);
 
     pulse = 1;
   }, "5n");
@@ -61,6 +60,7 @@ window.addEventListener("load", () => {
   //Following lines 52-53 are copied from: https://tonejs.github.io/docs/14.7.58/UserMedia
   meter = new Tone.Meter();
   mic = new Tone.UserMedia().connect(meter);
+  let level = meter.getValue();
 });
 
 //---------CLICK---------------
@@ -79,19 +79,35 @@ window.addEventListener("click", () => {
     console.log(currentSound);
   }
 });
-
-// --------DRAWING---------------
-function draw() {
-  background(0);
-  noiseSeed(1);
-  noStroke();
-  fill(0);
+//--------------SIZE------------
+function sizeChanges() {
+  if (soundTypes) {
+    if (soundTypes[currentSound] === "sine2") {
+      size = 15;
+      newArt = false;
+      nextArt = false;
+      lastArt = true;
+    } else if (soundTypes[currentSound] === "fatsawtooth") {
+      size = 5;
+      newArt = false;
+      nextArt = false;
+      lastArt = false;
+    } else if (soundTypes[currentSound] === "pulse") {
+      size = 8;
+      newArt = true;
+      nextArt = false;
+      lastArt = false;
+    } else if (soundTypes[currentSound] === "fmsquare") {
+      size = 10;
+      newArt = false;
+      nextArt = true;
+      lastArt = false;
+    }
+  }
+}
+//----------SOUND AND DIVIDER----------
+function soundChanges() {
   let level = meter.getValue();
-  let bwMode = level > -20;
-  const startX = width / 2 - (numCols * size) / 2;
-  const startY = height / 2 - (numRows * size) / 2;
-  const pulseStrength = 1 + pulse * 0.6;
-
   // Changing the divider according to microphone
   if (meter) {
     // level = meter.getValue();
@@ -109,26 +125,14 @@ function draw() {
       console.log("4th", level);
     }
   }
-  if (soundTypes) {
-    if (soundTypes[currentSound] === "sine2") {
-      size = 1;
-    } else if (soundTypes[currentSound] === "fatsine") {
-      size = 5;
-    } else if (soundTypes[currentSound] === "fatsawtooth") {
-      size = 10;
-    } else if (soundTypes[currentSound] === "pulse") {
-      size = 3;
-    } else if (soundTypes[currentSound] === "fmsquare") {
-      size = 8;
-    } else if (soundTypes[currentSound] === "sine") {
-      size = 2;
-    }
-  }
+}
 
-  numCols = ceil((320 / size) * 2);
-  numRows = ceil((320 / size) * 2);
-
-  // The following code was made with inspiration from the noise tutorial shown during one of the lectures.
+//-----------NOISE RECTANGLES----------
+function noisefield() {
+  let bwMode = level > -20;
+  const startX = width / 2 - (numCols * size) / 2;
+  const startY = height / 2 - (numRows * size) / 2;
+  const pulseStrength = 1 + pulse * 0.6;
   for (let y = 0; y < numRows; y++) {
     for (let x = 0; x < numCols; x++) {
       const value =
@@ -137,29 +141,76 @@ function draw() {
       // Wave effect
       const offsetX = noise(x / 15, y / 15, counter) * 20 - 10;
       const offsetY = noise(x / 15, y / 15, counter + 100) * 20 - 10;
-
       if (bwMode) {
         fill(255);
       } else {
-        fill(color, 20, 90);
+        fill(c, 20, 90);
       }
-
-      square(startX + x * size + offsetX, startY + y * size + offsetY, value);
+      if (newArt) {
+        let pointX = startX + x * size + offsetX;
+        let pointY = startY + y * size + offsetY;
+        const centerX = width / 2;
+        const centerY = height / 2;
+        const dinstance = dist(pointX, pointY, centerX, centerY);
+        let shift = map(dinstance, 0, (numCols * size) / 2, shuffel, 0);
+        let angle = noise(x, y, counter) * TWO_PI * 2;
+        let shiftY = cos(angle) * shift * 0.02;
+        let shiftX = sin(angle) * shift * 0.02;
+        fill(c, 80, 90);
+        square(pointX + shiftX, pointY + shiftY, value);
+      } else if (nextArt) {
+        let pointX = startX + x * size + offsetX;
+        let pointY = startY + y * size + offsetY;
+        const centerX = width / 2;
+        const centerY = height / 2;
+        const dinstance = dist(pointX, pointY, centerX, centerY);
+        let shift = map(dinstance, 0, (numCols * size) / 2, shuffel, 0);
+        let angle = noise(x, y, counter) * TWO_PI * 2;
+        let shiftY = cos(angle) * shift * 0.1;
+        let shiftX = sin(angle) * shift * 0.1;
+        fill(c, 100, 90);
+        square(pointX + shiftX, pointY + shiftY, value);
+      } else if (lastArt) {
+        let pointX = startX + x * size + offsetX;
+        let pointY = startY + y * size + offsetY;
+        const centerX = width / 2;
+        const centerY = height / 2;
+        const dinstance = dist(pointX, pointY, centerX, centerY);
+        let shift = map(dinstance, 0, (numCols * size) / 2, shuffel, 0);
+        let angle = noise(x, y, counter) * TWO_PI * 2;
+        let shiftY = cos(angle) * shift * 0.1;
+        let shiftX = sin(angle) * shift * 0.1;
+        fill(c, 120, 90);
+        square(pointX + shiftX, pointY + shiftY, value);
+      } else {
+        square(startX + x * size + offsetX, startY + y * size + offsetY, value);
+      }
+    }
+    if (bwMode) {
+      let numGlitches = int(random(5, 15));
+      for (let i = 0; i < numGlitches; i++) {
+        let glitchY = random(height);
+        let glitchH = random(10, 30);
+        let offsetX = random(-50, 50);
+        copy(0, glitchY, width, glitchH, offsetX, glitchY, width, glitchH);
+      }
     }
   }
 
-  counter += 0.05;
   pulse *= 0.6;
+  counter += 0.05;
+}
+// --------DRAWING---------------
+function draw() {
+  background(0);
+  noiseSeed(1);
+  noStroke();
+  fill(0);
 
-  // Glitch effect on loud microphone sound
-  // Lines 131 & 136 were written with help from ChatGPT
-  if (bwMode) {
-    let numGlitches = int(random(5, 15));
-    for (let i = 0; i < numGlitches; i++) {
-      let glitchY = random(height);
-      let glitchH = random(10, 30);
-      let offsetX = random(-50, 50);
-      copy(0, glitchY, width, glitchH, offsetX, glitchY, width, glitchH);
-    }
-  }
+  level = meter.getValue();
+  soundChanges();
+  sizeChanges();
+  numCols = ceil((300 / size) * 2);
+  numRows = ceil((300 / size) * 2);
+  noisefield();
 }
